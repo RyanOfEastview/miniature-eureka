34def29c3fb55063cd48f001e7b793861273b537
RyanOfEastview <ryanmccarthy613@gmail.com>
2022-09-11 15:01:39 -0400

    looks complete but with bugs - testing mode begins

--- routes/routes.js
+++ routes/routes.js
@@ -3,31 +3,54 @@ const path = require('path');
 
 module.exports = app = {
 
-    //API Routes
-    //Get Notes
-    app.get("/api/notes", function (req, res) {
-        res.json(notes);
-    });
+    fs.readFile("db/db.json","utf8", (err, data) => {
 
-    //Post Notes
-    app.post("/api/notes", function(req, res) {
-        let newNote = req.body;
-        notes.push(newNote);
-        updateDb();
-        return console.log("New Note Added!");
-    });
+        if (err) throw err;
+        var notes = JSON.parse(data);
 
-    //Get Specific Note
-    app.get("/api/notes/:id", function(req,res) {
-        res.json(notes[req.params.id]);
-    });
+        //API Routes
+        //Get Notes
+        app.get("/api/notes", function (req, res) {
+            res.json(notes);
+        });
 
-    // Delete a Note 
-    app.delete("/api/notes/:id", function(req, res) {
-        notes.splice(req.params.id, 1);
-        updateDb();
-        console.log("Note Deleted!");
-    });
+        //Post Notes
+        app.post("/api/notes", function(req, res) {
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log("New Note Added!");
+        });
 
-    
+        //Get Specific Note
+        app.get("/api/notes/:id", function(req,res) {
+            res.json(notes[req.params.id]);
+        });
+
+        //Delete a Note 
+        app.delete("/api/notes/:id", function(req, res) {
+            notes.splice(req.params.id, 1);
+            updateDb();
+            console.log("Note Deleted!");
+        });
+
+
+        //Show Posted Notes
+        app.get('/notes', function(req, res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+
+        //Show All Notes
+        app.get('*', function(req,res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+        //Updates Notes When Changes Made
+        function updateDb() {
+            fs.writeFile("db/db.json", JSON.stringify(notes, '/t'), err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+    });
 }
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const fs = require("fs");
-const path = require('path'):
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname));
 
-app.listen(PORT, function() {
-    console.log("Now listening!");
+require("./routes/routes")(app);
+
+app.listen(PORT, function () {
+  console.log("Now listening!");
 });
